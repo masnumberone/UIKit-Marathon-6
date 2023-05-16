4208eb578ae1bb2c984e9561d4e8b3da64d1b5e6
masnumberone <m.myxortov2011@gmail.com>
2023-05-16 20:31:59 +0300

    Initial Commit

---
+++ UIKit-Marathon-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-Marathon-6
+//
+//  Created by work on 16.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
